d120c79038d11522104b3b4328428380f597f124
Andre Valdestilhas <valdestilhas@informatik.uni-leipzig.de>
2018-12-18 11:55:13 +0100

    cleaning the repository

--- src/org/wimu/datasetselection/bgploadalot/Approach.java
+++
@@ -1,8 +0,0 @@
-package org.wimu.datasetselection.bgploadalot;
-
-public class Approach {
-	public static final String ENDPOINT = "ENDPOINT";
-	public static final String SPARQL_A_LOT = "SPARQL_A_LOT";
-	public static final String WIMU_DUMP = "WIMU_DUMP";
-	public static final String TRAVERSAL = "TRAVERSAL";
-}
